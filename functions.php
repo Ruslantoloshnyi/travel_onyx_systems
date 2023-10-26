eb86b7cb6817de393c14ad628b82f1f3b1e7a7f8
Ruslan <ru@skylinehealthgroup.com>
2023-10-26 15:38:14 +0300

    connect sri[t and styles into functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Travel onyx systems functions and definitions
  *
@@ -7,9 +8,10 @@
  * @package Travel_onyx_systems
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION'))
+{
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +21,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function tos_setup() {
+function tos_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on Travel onyx systems, use a find and replace
 		* to change 'tos' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'tos', get_template_directory() . '/languages' );
+	load_theme_textdomain('tos', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +40,19 @@ function tos_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'tos' ),
+			'menu-1' => esc_html__('Primary', 'tos'),
 		)
 	);
 
@@ -83,7 +86,7 @@ function tos_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +103,7 @@ function tos_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'tos_setup' );
+add_action('after_setup_theme', 'tos_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +112,24 @@ add_action( 'after_setup_theme', 'tos_setup' );
  *
  * @global int $content_width
  */
-function tos_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'tos_content_width', 640 );
+function tos_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('tos_content_width', 640);
 }
-add_action( 'after_setup_theme', 'tos_content_width', 0 );
+add_action('after_setup_theme', 'tos_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function tos_widgets_init() {
+function tos_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'tos' ),
+			'name'          => esc_html__('Sidebar', 'tos'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'tos' ),
+			'description'   => esc_html__('Add widgets here.', 'tos'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +137,25 @@ function tos_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'tos_widgets_init' );
+add_action('widgets_init', 'tos_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function tos_scripts() {
-	wp_enqueue_style( 'tos-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'tos-style', 'rtl', 'replace' );
+function tos_scripts()
+{
+	wp_enqueue_style('tos-style', get_template_directory_uri() . './assets/css/style.css');
 
-	wp_enqueue_script( 'tos-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	wp_enqueue_script('tos-scripts', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), _S_VERSION, true);
+
+
+	if (is_singular() && comments_open() && get_option('thread_comments'))
+	{
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'tos_scripts' );
+add_action('wp_enqueue_scripts', 'tos_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +180,7 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION'))
+{
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
