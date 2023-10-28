73cffc88386d1e3133e40964d7c9bd4d2b97be74
Ruslan <ru@skylinehealthgroup.com>
2023-10-29 00:12:36 +0300

    connect fonts

--- header-dest.php
+++ header-dest.php
@@ -13,7 +13,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 
     <?php wp_head(); ?>
 </head>
